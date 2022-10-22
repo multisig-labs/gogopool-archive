9e07c5b790908493e42ea429bbbd1a5a4cb5bf83
Johnny Gault <johnnygault@protonmail.com>
2022-10-22 11:49:14 -0700

    ProtocolDAOClaim test

--- contracts/contract/rewards/claims/ProtocolDAOClaim.sol
+++ contracts/contract/rewards/claims/ProtocolDAOClaim.sol
@@ -3,39 +3,36 @@ pragma solidity ^0.8.13;
 // SPDX-License-Identifier: GPL-3.0-only
 
 import "../../Base.sol";
-import {Storage} from "../../Storage.sol";
-import {Vault} from "../../Vault.sol";
-import {TokenGGP} from "../../tokens/TokenGGP.sol";
 import {ProtocolDAO} from "../../dao/ProtocolDAO.sol";
+import {Storage} from "../../Storage.sol";
+import {TokenGGP} from "../../tokens/TokenGGP.sol";
+import {Vault} from "../../Vault.sol";
 
-// RPL Rewards claiming by the DAO
 contract ProtocolDAOClaim is Base {
-	event GGPTokensSentByDAOProtocol(string invoiceID, address indexed from, address indexed to, uint256 amount, uint256 time);
+	error ErrorSendingAVAX();
+	error InvalidAmount();
+
+	event GGPTokensSentByDAOProtocol(string invoiceID, address indexed from, address indexed to, uint256 amount);
 
 	constructor(Storage storageAddress) Base(storageAddress) {
-		// Version
 		version = 1;
 	}
 
-	// Spend the network DAOs RPL rewards
-	// todo add onlyLatestContract("rocketDAOProtocolProposals", msg.sender)
+	// Spend the ProtocolDAOs GGP rewards
 	function spend(
-		string memory _invoiceID,
-		address _recipientAddress,
-		uint256 _amount
-	) external {
-		// Load contracts
+		string memory invoiceID,
+		address recipientAddress,
+		uint256 amount
+	) external onlyGuardian {
 		Vault vault = Vault(getContractAddress("Vault"));
-		// Addresses
 		TokenGGP ggpToken = TokenGGP(getContractAddress("TokenGGP"));
-		// Some initial checks
-		require(
-			_amount > 0 && _amount <= vault.balanceOfToken("ProtocolDAOClaim", ggpToken),
-			"You cannot send 0 GGP or more than the DAO has in its account"
-		);
-		// Send now
-		vault.withdrawToken(_recipientAddress, ggpToken, _amount);
-		// Log it
-		emit GGPTokensSentByDAOProtocol(_invoiceID, address(this), _recipientAddress, _amount, block.timestamp);
+
+		if (amount == 0 || amount > vault.balanceOfToken("ProtocolDAOClaim", ggpToken)) {
+			revert InvalidAmount();
+		}
+
+		vault.withdrawToken(recipientAddress, ggpToken, amount);
+
+		emit GGPTokensSentByDAOProtocol(invoiceID, address(this), recipientAddress, amount);
 	}
 }
---
+++ test/unit/ProtocolDAOClaim.t.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.8.13;
+
+// SPDX-License-Identifier: GPL-3.0-only
+
+import "./utils/BaseTest.sol";
+import {ProtocolDAO} from "../../contracts/contract/dao/ProtocolDAO.sol";
+
+contract ProtocolDAOClaimTest is BaseTest {
+	function setUp() public override {
+		super.setUp();
+
+		vm.startPrank(guardian, guardian);
+		ggp.approve(address(vault), 1000 ether);
+		vault.depositToken("ProtocolDAOClaim", ggp, 1000 ether);
+		vm.stopPrank();
+	}
+
+	function testSpendFunds() public {
+		address alice = getActor("alice");
+		// TODO This isnt working?
+		// vm.expectRevert(MultisigManager.MustBeGuardian.selector);
+		bytes memory customError = abi.encodeWithSignature("MustBeGuardian()");
+		vm.expectRevert(customError);
+		daoClaim.spend("Invoice1", alice, 100 ether);
+
+		vm.startPrank(guardian);
+		vm.expectRevert(ProtocolDAOClaim.InvalidAmount.selector);
+		daoClaim.spend("Invoice1", alice, 0 ether);
+
+		vm.expectRevert(ProtocolDAOClaim.InvalidAmount.selector);
+		daoClaim.spend("Invoice1", alice, 1001 ether);
+
+		daoClaim.spend("Invoice1", alice, 1000 ether);
+	}
+}
