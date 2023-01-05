9422322ced9c9716575834c049c9e242cfac1872
Johnny Gault <johnnygault@protonmail.com>
2023-01-05 11:48:09 -0800

    Remove ability to state change from Withdrawable to Prelaunch. New method recordStakingEndThenCycleThenClaim to prevent liq staker funds from being sniped

--- test/unit/TokenggAVAX.t.sol
+++ test/unit/TokenggAVAX.t.sol
@@ -188,7 +188,7 @@ contract TokenggAVAXTest is BaseTest, IWithdrawer {
 		uint256 endTime = block.timestamp + mp.duration;
 
 		skip(mp.duration);
-		minipoolMgr.recordStakingEnd{value: totalStakedAmount + rewardsAmount}(nodeID, endTime, rewardsAmount);
+		minipoolMgr.recordStakingEndThenMaybeCycle{value: totalStakedAmount + rewardsAmount}(nodeID, endTime, rewardsAmount);
 		vm.stopPrank();
 
 		assertEq(address(rialto).balance, rialtoInitBal);
@@ -277,7 +277,7 @@ contract TokenggAVAXTest is BaseTest, IWithdrawer {
 
 		vm.deal(address(rialto), address(rialto).balance + rewardsAmt);
 		vm.prank(address(rialto));
-		minipoolMgr.recordStakingEnd{value: nodeAmt + rewardsAmt}(mp.nodeID, block.timestamp, rewardsAmt);
+		minipoolMgr.recordStakingEndThenMaybeCycle{value: nodeAmt + rewardsAmt}(mp.nodeID, block.timestamp, rewardsAmt);
 
 		ggAVAX.syncRewards();
 		skip(ggAVAX.rewardsCycleLength());
