ef87b397cf2bda718b3036c53b687d582960ca12
Julie <julie@multisiglabs.org>
2023-01-31 17:22:41 +0200

    fixed the slashing duration and the slash amt if it is more than what the user has staked

--- contracts/contract/MinipoolManager.sol
+++ contracts/contract/MinipoolManager.sol
@@ -697,14 +697,19 @@ contract MinipoolManager is Base, ReentrancyGuard, IWithdrawer {
 	function slash(int256 index) private {
 		address nodeID = getAddress(keccak256(abi.encodePacked("minipool.item", index, ".nodeID")));
 		address owner = getAddress(keccak256(abi.encodePacked("minipool.item", index, ".owner")));
+		uint256 duration = block.timestamp - getUint(keccak256(abi.encodePacked("minipool.item", index, ".startTime")));
 		uint256 avaxLiquidStakerAmt = getUint(keccak256(abi.encodePacked("minipool.item", index, ".avaxLiquidStakerAmt")));
-		uint256 expectedAVAXRewardsAmt = getExpectedAVAXRewardsAmt(14 days, avaxLiquidStakerAmt);
+		uint256 expectedAVAXRewardsAmt = getExpectedAVAXRewardsAmt(duration, avaxLiquidStakerAmt);
 		uint256 slashGGPAmt = calculateGGPSlashAmt(expectedAVAXRewardsAmt);
+
+		Staking staking = Staking(getContractAddress("Staking"));
+		if (staking.getGGPStake(owner) < slashGGPAmt) {
+			slashGGPAmt = staking.getGGPStake(owner);
+		}
 		setUint(keccak256(abi.encodePacked("minipool.item", index, ".ggpSlashAmt")), slashGGPAmt);
 
 		emit GGPSlashed(nodeID, slashGGPAmt);
 
-		Staking staking = Staking(getContractAddress("Staking"));
 		staking.slashGGP(owner, slashGGPAmt);
 	}
 
