df81d85352516693c5f3203f8dee977a6ec74cfa
memerson <emerson.cloud@gmail.com>
2023-02-02 16:02:06 +0200

    Revert "C4-352, 450, 823, 673 - Evaluate pausing  (#230)" (#243)
    This reverts commit d889791b6ea534eae7581ea51e640ace5b79ec9b.

--- test/unit/RewardsPool.t.sol
+++ test/unit/RewardsPool.t.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.17;
 
 import "./utils/BaseTest.sol";
-import {BaseAbstract} from "../../contracts/contract/BaseAbstract.sol";
 import {FixedPointMathLib} from "@rari-capital/solmate/src/utils/FixedPointMathLib.sol";
 
 contract RewardsPoolTest is BaseTest {
@@ -160,40 +159,5 @@ contract RewardsPoolTest is BaseTest {
 		assertEq(ggp.balanceOf(multisig1), amtPerMultisig);
 		assertEq(ggp.balanceOf(multisig2), amtPerMultisig);
 		assertEq(ggp.balanceOf(multisig3), 0);
-	}
-
-	// When syncRewards is delayed, tokens should still inflate when next sync is called
-	function testInflationAmtWithRewardsDelay() public {
-		// skip two cycles before syncing rewards
-		skip(2 * dao.getRewardsCycleSeconds());
-
-		uint256 inflationIntervalsElapsed = 56;
-		uint256 inflationRate = dao.getInflationIntervalRate();
-		uint256 expectedInflationTokens = dao.getTotalGGPCirculatingSupply();
-		for (uint256 i = 0; i < inflationIntervalsElapsed; i++) {
-			expectedInflationTokens = expectedInflationTokens.mulWadDown(inflationRate);
-		}
-
-		// start rewards cycle
-		rewardsPool.startRewardsCycle();
-
-		// verify inflated tokens
-		assertEq(dao.getTotalGGPCirculatingSupply(), expectedInflationTokens);
-	}
-
-	function testStartRewardsCyclePaused() public {
-		skip(dao.getRewardsCycleSeconds());
-
-		assertEq(rewardsPool.getRewardsCyclesElapsed(), 1);
-
-		assertTrue(rewardsPool.canStartRewardsCycle());
-
-		vm.prank(address(ocyticus));
-		dao.pauseContract("RewardsPool");
-
-		assertFalse(rewardsPool.canStartRewardsCycle());
-
-		vm.expectRevert(BaseAbstract.ContractPaused.selector);
-		rewardsPool.startRewardsCycle();
 	}
 }
