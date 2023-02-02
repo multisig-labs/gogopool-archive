1ea260b239dcfa43a30dc04009b881fb9bcfc0d9
Emerson Cloud <emerson.cloud@gmail.com>
2023-02-02 10:37:08 +0200

    Add max deposit and mint functions

--- test/unit/TokenggAVAX.t.sol
+++ test/unit/TokenggAVAX.t.sol
@@ -434,6 +434,26 @@ contract TokenggAVAXTest is BaseTest, IWithdrawer {
 		assertEq(assets, 0);
 	}
 
+	function testMaxMint() public {
+		address liqStaker = getActor("liqStaker");
+		assertEq(ggAVAX.maxMint(liqStaker), type(uint256).max);
+
+		vm.prank(address(ocyticus));
+		dao.pauseContract("TokenggAVAX");
+
+		assertEq(ggAVAX.maxMint(liqStaker), 0);
+	}
+
+	function testMaxDeposit() public {
+		address liqStaker = getActor("liqStaker");
+		assertEq(ggAVAX.maxDeposit(liqStaker), type(uint256).max);
+
+		vm.prank(address(ocyticus));
+		dao.pauseContract("TokenggAVAX");
+
+		assertEq(ggAVAX.maxDeposit(liqStaker), 0);
+	}
+
 	function testMaxWithdrawPaused() public {
 		vm.prank(address(ocyticus));
 		dao.pauseContract("TokenggAVAX");
