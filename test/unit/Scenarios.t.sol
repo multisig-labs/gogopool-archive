6431a0953535b79902ae88c25d1f76205e4e7478
memerson <emerson.cloud@gmail.com>
2023-02-05 14:23:47 +0200

    Revert "C4-519: Fix cancelMinipool to be on minipool creation not rewards start time (#215)" (#269)
    This reverts commit 7138ae3f4fb1c973ab0159ffd22d7b49d3d98f53.

--- test/unit/Scenarios.t.sol
+++ test/unit/Scenarios.t.sol
@@ -495,8 +495,7 @@ contract ScenariosTest is BaseTest {
 
 		vm.startPrank(nodeOp1);
 		MinipoolManager.Minipool memory mp2 = createMinipool(depositAmt, depositAmt, duration);
-		assertEq(staking.getAVAXAssignedHighWater(nodeOp1), depositAmt);
-		skip(5 seconds); //cancel min time
+		assertEq(staking.getAVAXValidatingHighWater(nodeOp1), depositAmt);
 		minipoolMgr.cancelMinipool(mp2.nodeID);
 		vm.stopPrank();
 
